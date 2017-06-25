d6f41a751d927faf7fbcc670dc2424a0e08e0707
Igor Tynianko <fanrise@gmail.com>
2017-06-25 23:24:08 +0300

    Автоматизация (автопрефиксер, брузерсинх)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,35 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var plumber = require('gulp-plumber');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
 
+
+// gulp.task('start', function() {
+//     browserSync.init({
+//         server: {
+//             baseDir: "."
+//         }
+//     });
+// });
 gulp.task('less', function () {
   return gulp.src('less/style.less')
+    .pipe(plumber())
     .pipe(less())
-    .pipe(gulp.dest('css'));
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({stream:true}));
 });
+gulp.task('serve', ['less'], function() {
+    browserSync.init({
+        server: "."
+    });
+
+    gulp.watch('less/**/*.less', ['less']);
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+// gulp.task('default', ['serve']);
--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ burger.addEventListener("click", function(event) {
   event.preventDefault();
   header_closed.classList.toggle("main-header--closed");
 });
+
+
+
+var btn_first = document.querySelector(".radio__btn--first")
+var btn_second = document.querySelector(".radio__btn--second")
+var btn_third = document.querySelector(".radio__btn--third")
+// var rev1 = document.querySelector(".reviews__item:nth-child(1)")
+// var rev2 = document.querySelector(".reviews__item:nth-child(2)")
+// var rev3 = document.querySelector(".reviews__item:nth-child(3)")
+var rev = document.querySelector(".reviews__item-container")
+
+
+btn_first.addEventListener("click", function(event) {
+  rev.classList.add("reviews__item-container--first");
+  rev.classList.remove("reviews__item-container--second");
+  rev.classList.remove("reviews__item-container--third");
+});
+btn_second.addEventListener("click", function(event) {
+  rev.classList.remove("reviews__item-container--first");
+  rev.classList.add("reviews__item-container--second");
+  rev.classList.remove("reviews__item-container--third");
+});
+btn_third.addEventListener("click", function(event) {
+  rev.classList.remove("reviews__item-container--first");
+  rev.classList.remove("reviews__item-container--second");
+  rev.classList.add("reviews__item-container--third");
+});
